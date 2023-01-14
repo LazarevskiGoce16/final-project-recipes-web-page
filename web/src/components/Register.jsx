cf3f208a482a3c48f3c3ada67c4608ebe61ccbc0
goce <lazarevskigoce26@gmail.com>
2023-01-14 22:39:03 +0100

    updated fonts and bullets

--- web/src/components/Register.jsx
+++ web/src/components/Register.jsx
@@ -1,5 +1,19 @@
+import { Header } from './Header';
+import { Footer } from './Footer';
+import '../css/Register.css';
+
 export const Register = () => {
     return (
-        <h1>Register Page</h1>
+        <>
+        <Header />
+        <div className='register-body'>
+            <div className='register-wrapper'>
+                <div className='register-wrapper'>
+                    <h1>Create Account</h1>
+                </div>
+            </div>
+        </div>
+        <Footer />
+        </>
     )
 };
