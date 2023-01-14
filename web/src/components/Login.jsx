0fabb191aefee45f161830e021e9f8cc6410ece7
goce <lazarevskigoce26@gmail.com>
2023-01-14 21:42:44 +0100

    created and styled the login page

--- web/src/components/Login.jsx
+++ web/src/components/Login.jsx
@@ -1,5 +1,39 @@
+import { Header } from './Header';
+import { Footer } from './Footer';
+import '../css/Login.css';
+
 export const Login = () => {
     return (
-        <h1>Login Page</h1>
+        <>
+        <Header />
+        <div className='login-body'>
+            <div className='login-wrapper'>
+                <div className='login-title'>
+                    <h1>Log In</h1>
+                </div>
+                <div className='login-content'>
+                    <div className='login-left-content'>
+                        <h1>
+                            <span style={{color: "#F0972A"}}>Welcome to </span>
+                            <span style={{color: "#626262"}}>Baby's</span>
+                        </h1>
+                        <p>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+                    </div>
+                    <div className='login-right-content'>
+                        <label htmlFor="">Email</label>
+                        <br />
+                        <input type="text" name="email" className='email-input' placeholder='user@domain.com'/>
+                        <br />
+                        <label htmlFor="">Password</label>
+                        <br />
+                        <input type="password" name="password" className='password-input' placeholder='********'/>
+                        <br />
+                        <button className='login-button'>LOG IN</button>
+                    </div>
+                </div> 
+            </div>
+        </div>
+        <Footer />
+        </>
     )
 };
