7cdaf3cea04b10710725f04832de448d8c750d11
goce <lazarevskigoce26@gmail.com>
2023-01-25 00:56:57 +0100

    created and styled all the category pages

--- web/src/components/Brunch.jsx
+++ web/src/components/Brunch.jsx
@@ -1,5 +1,62 @@
+import axios from "axios";
+import { Card } from "./Card";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { useState, useEffect } from "react";
+import "../css/Categories.css";
+
 export const Brunch = () => {
+    const cardBody = 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal';
+    const bkgImgUrl = 'https://www.nourishedlife.co.uk/media/qm4js31t/pizza-beer-1200x628-facebook-1200x628.jpg?width=500&height=261.6666666666667';
+
+    const [category, setCategory] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:10003/api/v1/auth/recipes/category', {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",  
+            }
+        })
+        .then(res => {
+            let arr = [];
+            for (let i = 0; i < res.data.rs.length; i++) {
+                if(res.data.rs[i].category === 'brunch') {
+                    arr.push(res.data.rs[i]);
+                }
+            }
+            setCategory(arr);
+            console.log(res.data.rs);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, []);
+
     return (
-        <h1>Brunch Page</h1>
+        <>
+        <Header />
+        <div className="all-categories-body">
+            <div className="all-categories-wrapper">
+                <div className="all-categories-title brunch-title">
+                    <h1>Brunch</h1>
+                </div>
+                <div className="all-categories-content">
+                    {category.map(c => (
+                        <Card 
+                            title={c.title}
+                            imageUrl={bkgImgUrl}
+                            body={cardBody}
+                            courseType={c.category}
+                            stars={c.stars}
+                            persons={c.num_of_people}
+                            time={c.preparation_time}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+        <Footer />
+        </>
     )
 };
